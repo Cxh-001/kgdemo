bdf38ac808ed70343b35d80508a90900c335c499
unknown <915890515@qq.com>
2020-01-22 19:47:31 +0800

    Updates

---
+++ precache-manifest.f1aec3513f7fdc97e1229fabe749ff7e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "366f4f31eb3511ff27754ef8e51eafd6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e3c1a818907a71daee74",
+    "url": "/static/css/2.2bc43135.chunk.css"
+  },
+  {
+    "revision": "db73b4dce9048c41d018",
+    "url": "/static/css/main.d2b4f029.chunk.css"
+  },
+  {
+    "revision": "e3c1a818907a71daee74",
+    "url": "/static/js/2.e9db50d3.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/static/js/2.e9db50d3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "db73b4dce9048c41d018",
+    "url": "/static/js/main.a268b04c.chunk.js"
+  },
+  {
+    "revision": "4dedda09750c20390431",
+    "url": "/static/js/runtime-main.fc299c43.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  }
+]);
